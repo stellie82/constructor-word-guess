6b08757831a983f91c8a129cacec0ee91c65448a
stellie82 <kims82@gmail.com>
2019-11-04 12:47:09 -0800

    add inquirer function

--- letter.js
+++ letter.js
@@ -5,14 +5,28 @@
 // A boolean value that stores whether that letter has been guessed yet
 // A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
 // A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+
 var validLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var userInput = process.argv[2];
 var guessedLetter = ["H"];
 var guessed = false;
 
 // WORD
-var randomMovie = "The Wave";
+var movies = [
+    "The Wave",
+    "Armageddon",
+    "San Andreas",
+    "The Day After Tomorrow",
+    "Titanic",
+    "Contagion",
+    "Twister"
+];
+
 var movieArray;
+var randomMovie = movies[Math.floor(Math.random() * movies.length)];
 var guessesArray = [];
 
 function parseMovie(word) {
@@ -22,11 +36,24 @@ function parseMovie(word) {
             movieArray.push("__");
         } else if (word[i] === " ") {
             movieArray.push(" ");
-        }  
+        }
     }
     console.log(movieArray.join(" "));
 }
 
 
-// let random = Math.floor(Math.random() * word.length);
-
+inquirer
+    .prompt([
+        {
+            name: "letter",
+            message: "Guess a letter!"
+        }
+    ])
+    .then(function (response) {
+        if (response) {
+            console.log("\nYou guessed the letter: " + response.letter.toUpperCase());
+        }
+        else {
+            console.log("\nSomething else\n");
+        }
+    });
